8ea98f14403b4914aaaa57a16dcd1d0cdcd35f67
Volodymyr <ymyr.vlad@gmail.com>
2024-03-15 23:02:19 +0200

    [2] connect postgresql

--- php.dockerfile
+++ php.dockerfile
@@ -4,4 +4,5 @@ RUN mkdir -p /var/www/html
 
 RUN apk --no-cache add shadow && usermod -u 1000 www-data
 
-RUN docker-php-ext-install pdo pdo_mysql
+RUN apk --no-cache add postgresql-dev \
+    && docker-php-ext-install pdo intl pdo_pgsql
--- src/config/database.php
+++ src/config/database.php
@@ -88,7 +88,7 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => 'laravel',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
