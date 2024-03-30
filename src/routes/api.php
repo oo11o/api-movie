2499ea87e794840934184b3120c82babd1b5a04d
Volodymyr <ymyr.vlad@gmail.com>
2024-03-30 15:58:21 +0200

    [TASK-6] add api version

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\MovieController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies', function (){
+});
